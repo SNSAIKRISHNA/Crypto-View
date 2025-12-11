d981a2f33d322b2426d45220792287b39c781e70
SNSAIKRISHNA <saikrishnasnoff@gmail.com>
2025-12-11 10:39:28 +0530

    commit-2

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import SearchBar from "./Components/SearchBar";
+import MovieList from "./Components/MovieList";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <SearchBar />
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+        <nav className="Nav-bar">
+            <h1>Crypto</h1>
+            
+        </nav>
+    )
+}
